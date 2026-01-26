096d149d1447184828255606601dfef403302ef4
xtbg-thn <ethan.bounour-norest@universite-paris-saclay.fr>
2026-01-26 17:13:12 +0100

    Exemple 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-  int test;
-  test=1;
+void maTache(void *parametres)
+{
+  while (1) // boucle infinie
+  {
+    Serial.printf("%s %08X Core = %d\n", 
+      pcTaskGetName(NULL), 
+      (int)xTaskGetCurrentTaskHandle(),
+      xPortGetCoreID());
+    
+    delay(1000);
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  int priorite = 0;
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  Serial.printf("Départ\n");
+  xTaskCreatePinnedToCore(maTache, "Ma tâche", 10000, NULL, 1, NULL, 1);
+  //xTaskCreatePinnedToCore(maTache,)
+  //priorite = uxTaskPriorityGet(handle);
+}
+void loop()
+{
+  Serial.printf("%s %08X Core = %d\n", 
+    pcTaskGetName(NULL), 
+    (int)xTaskGetCurrentTaskHandle(),
+    xPortGetCoreID());
+  delay(1000);
 }
