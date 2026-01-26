d02ed08ab7ed224e29a64abc8a5473f99d5c0184
xtbg-thn <ethan.bounour-norest@universite-paris-saclay.fr>
2026-01-26 17:30:34 +0100

    Exemple 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,54 @@
 #include <Arduino.h>
-void maTache(void *parametres)
+void maTache1(void *parametres)
 {
+  int i1 = 0;
   while (1) // boucle infinie
   {
-    Serial.printf("%s %08X Core = %d\n", 
-      pcTaskGetName(NULL), 
-      (int)xTaskGetCurrentTaskHandle(),
-      xPortGetCoreID());
-    
+    Serial.printf("maTache1 %4d\n", i1++);
     delay(1000);
+    if (i1==5){
+      vTaskSuspend(NULL);
+    }    
+  }
+}
+void maTache2(void *parametres)
+{
+  int i2 = 0;
+  while (1) // boucle infinie
+  {
+    Serial.printf("maTache2 %4d\n", i2++);
+    delay(4000);*
+    if (il==10){
+      vTaskDelete(NULL);
+    } 
   }
 }
 void setup()
 {
-  int priorite = 0;
   Serial.begin(115200);
   while (!Serial)
     ;
   Serial.printf("Départ\n");
-  xTaskCreatePinnedToCore(maTache, "Ma tâche", 10000, NULL, 1, NULL, 1);
-  //xTaskCreatePinnedToCore(maTache,)
-  //priorite = uxTaskPriorityGet(handle);
+  xTaskCreate(
+      maTache1,     /* Fonction de la tâche. */
+      "Ma tâche 1", /* Nom de la tâche. */
+      10000,        /* Taille de la pile de la tâche */
+      NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
+      1,            /* Priorité de la tâche */
+      NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
+            optionnel */
+  xTaskCreate(
+      maTache2,     /* Fonction de la tâche. */
+      "Ma tâche 2", /* Nom de la tâche. */
+      10000,        /* Taille de la pile de la tâche */
+      NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
+      1,            /* Priorité de la tâche */
+      NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
+            optionnel */
 }
 void loop()
 {
-  Serial.printf("%s %08X Core = %d\n", 
-    pcTaskGetName(NULL), 
-    (int)xTaskGetCurrentTaskHandle(),
-    xPortGetCoreID());
+  static int il = 0;
+  Serial.printf("Loop %4d\n", il++);
   delay(1000);
 }
