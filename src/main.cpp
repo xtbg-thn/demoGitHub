491163e3732aee0dd70e56451df27d9348d8371d
xtbg-thn <ethan.bounour-norest@universite-paris-saclay.fr>
2026-01-28 09:18:04 +0100

    Exemple 2 fin

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+TaskHandle_t h1;
+TaskHandle_t h2;
 void maTache1(void *parametres)
 {
   int i1 = 0;
@@ -6,9 +8,10 @@ void maTache1(void *parametres)
   {
     Serial.printf("maTache1 %4d\n", i1++);
     delay(1000);
-    if (i1==5){
+    if (i1 == 5)
+    {
       vTaskSuspend(NULL);
-    }    
+    }
   }
 }
 void maTache2(void *parametres)
@@ -17,10 +20,8 @@ void maTache2(void *parametres)
   while (1) // boucle infinie
   {
     Serial.printf("maTache2 %4d\n", i2++);
-    delay(4000);*
-    if (il==10){
-      vTaskDelete(NULL);
-    } 
+
+    delay(4000);
   }
 }
 void setup()
@@ -35,20 +36,26 @@ void setup()
       10000,        /* Taille de la pile de la tâche */
       NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
       1,            /* Priorité de la tâche */
-      NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
-            optionnel */
+      &h1);         /* passer l'adresse du handle */
   xTaskCreate(
       maTache2,     /* Fonction de la tâche. */
       "Ma tâche 2", /* Nom de la tâche. */
       10000,        /* Taille de la pile de la tâche */
       NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
       1,            /* Priorité de la tâche */
-      NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
-            optionnel */
+      &h2);         /* passer l'adresse du handle */
 }
 void loop()
 {
-  static int il = 0;
+  static int il = 0;                // persist across loops
   Serial.printf("Loop %4d\n", il++);
+  if (il == 10)
+  {
+    vTaskDelete(h2);
+  }
+  else if (il == 15)
+  {
+    vTaskResume(h1);
+  }
   delay(1000);
 }
