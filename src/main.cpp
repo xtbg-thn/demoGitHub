34aebda84dc21fd1009ec1cf54f2cd4c0508eb97
xtbg-thn <ethan.bounour-norest@universite-paris-saclay.fr>
2026-01-28 11:06:07 +0100

    Exemple 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,67 @@
 #include <Arduino.h>
-void maTache(void *parametres)
+// Handle de la queue
+xQueueHandle queue;
+void tacheEnvoi(void *parametres)
 {
- int v1 = 0;
- static int v2 = 0;
- while (1) // boucle infinie
- {
- Serial.printf("%s : v1=%d v2=%d\n", pcTaskGetName(NULL), v1, v2);
- v1++;
- v2++;
- delay(500);
- }
+  int i = 100;
+  while (1)
+  {
+    if (xQueueSend(queue, &i, portMAX_DELAY) == pdPASS)
+    {
+      Serial.printf("Envoi %d\n", i);
+      i++;
+    }
+    else
+    {
+      Serial.printf("Envoi échec\n");
+    }
+    delay(2000);
+  }
+}
+void tacheReception(void *parametres)
+{
+  int i;
+  while (1)
+  {
+    if (xQueueReceive(queue, &i, 0) != pdTRUE)
+    {
+      Serial.printf("Réception échec\n");
+    }
+    else
+    {
+      Serial.printf("Réception %d\n", i);
+    }
+    delay(1000);
+  }
 }
 void setup()
 {
   Serial.begin(115200);
-  Serial.printf("Initialisation\n");
-  //pinMode(16, OUTPUT); // configure P1.16 en sortie
-  // Création de la tâche périodique
-  xTaskCreate(maTache, "Tâche 1", 10000, NULL, 2, NULL);
-  xTaskCreate(maTache, "Tâche 2", 10000, NULL, 2, NULL);
-
+  while (!Serial)
+    ;
+  Serial.printf("Départ\n");
+  // Création de la file
+  queue = xQueueCreate(10, sizeof(int));
+  // La queue permet a donne une limite de fois avant de bloquer l'envoi ex: si la queue atteint 10 éléments, l'envoi sera bloqué jusqu'à ce qu'un élément soit reçu
+  xTaskCreate(
+      tacheEnvoi, /* Fonction de la tâche. */
+      "Envoi",    /* Nom de la tâche. */
+      10000,      /* Taille de la pile de la tâche */
+      NULL,       /* Paramètres de la tâche, NULL si pas de paramètre */
+      1,          /* Priorité de la tâche */
+      NULL);      /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
+  xTaskCreate(
+      tacheReception, /* Fonction de la tâche. */
+      "Réception",    /* Nom de la tâche. */
+      10000,          /* Taille de la pile de la tâche */
+      NULL,           /* Paramètres de la tâche, NULL si pas de paramètre */
+      1,              /* Priorité de la tâche */
+      NULL);          /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
+  vTaskDelete(NULL); // Supprime la tâche en cours quand le queue est pleine
 }
 void loop()
 {
-  //static int i = 0;
-  //Serial.printf("Boucle principale : %d\n", i++);
-  delay(1000);
+  // Ne s'exécute pas
 }
-// v1 s'incrémente 1 fois à chaque itération de la tâche
-// v2 s'incrémente 2 fois à chaque itération de la tâche
+// dabord on envoie et attend de recevoir jusqu'à ce que la queue soit pleine et on change la taille de la queue
+// ensuite on peut continuer a tester sans attendre l'envoie
